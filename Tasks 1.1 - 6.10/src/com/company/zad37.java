aeb9e1f95ee5bd34acbc73a652b5bf41daba35b7
Dmitrii <matdn2001@gmail.com>
2020-09-17 00:59:28 +0300

    3.7

--- Tasks 1.1 - 6.10/src/com/company/zad37.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class zad37 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ Tasks 1.1 - 6.10/src/com/company/zad37✔.java
@@ -0,0 +1,45 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class zad37 {
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите число: ");
+        String s = reader.readLine();
+        int n = Integer.parseInt(s);
+        int n2 = n * n;
+        int len = Integer.toString(n2).length();
+        System.out.print(ANSI_YELLOW + isKaprekar(n, n2, len) + ANSI_RESET);
+    }
+    public static String isKaprekar(int n, int k, int len){
+        String left = "", right = "", v = "";
+        if (len == 1){
+            right = Integer.toString(k);
+            if (Integer.parseInt(right) == n)
+                v = "Число " + n + " является числом Капрекара";
+            else
+                v = "Число " + n + " не является числом Капрекара";;
+        }
+        else if (len % 2 == 0){
+            left = Integer.toString(k).substring(0, len / 2);
+            right = Integer.toString(k).substring(len / 2);
+            if (Integer.parseInt(left) + Integer.parseInt(right) == n)
+                v = "Число " + n + " является числом Капрекара";
+            else
+                v = "Число " + n + " не является числом Капрекара";
+        }
+        else{
+            for (int i = 0; i < len / 2; i++){
+                left += Integer.toString(k).charAt(i);
+                if ((Integer.parseInt(left) + Integer.parseInt(Integer.toString(k).substring(i + 1))) == n)
+                    v = "Число " + n + " является числом Капрекара";
+                else
+                    v = "Число " + n + " не является числом Капрекара";
+            }
+        }
+        return v;
+    }
+}
