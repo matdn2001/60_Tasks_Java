089bed6495509e4b733b11dd4ff00c26a7282ff1
Dmitrii <matdn2001@gmail.com>
2020-10-12 14:54:54 +0300

    12.10.2020 Update

---
+++ Tasks 1.1 - 6.10/src/com/company/zad610✔.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class zad610 {
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите число: ");
+        String s = reader.readLine();
+        System.out.println(ANSI_YELLOW + palindromedescedent(s));
+    }
+    public static boolean isPalindrome(String s){
+        String backward = "";
+        for (int i = s.length() - 1; i >= 0; i--){
+            backward += s.charAt(i);
+        }
+        if (s.equals(backward))
+            return true;
+        else
+            return false;
+    }
+    public static String palindromedescedent(String s){
+        if (isPalindrome(s)){
+            return "Число " + s + " является палиндромом";
+        }
+        else{
+            System.out.print(ANSI_YELLOW + "Потомки этого числа: ");
+            String newS = s;
+            while (!isPalindrome(newS)){
+                int k = newS.length() / 2;
+                if (k == 0)
+                    return "Число " + newS + " является палиндромом";
+                int[] potomok = new int[k];
+                int j = 0;
+                for (int i = 0; i < k; i++){
+                    potomok[i] = Integer.parseInt(String.valueOf(newS.charAt(j))) + Integer.parseInt(String.valueOf(newS.charAt(j + 1)));
+                    j += 2;
+                }
+                newS = "";
+                for (int i = 0; i < potomok.length; i++){
+                    newS += Integer.toString(potomok[i]);
+                }
+                System.out.print(newS + " ");
+            }
+            System.out.println();
+            return "Число " + newS + " является потомком числа " + s + " и является палиндромом";
+        }
+    }
+}
--- Tasks 1.1 - 6.10/src/com/company/zad64.java
+++ Tasks 1.1 - 6.10/src/com/company/zad64.java
@@ -1,4 +1,11 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class zad64 {
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    }
 }
